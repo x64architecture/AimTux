1157f432972993cdef7b9f4c03aa28f0adaf10c6
SirBarclay <barclaycsgo@gmail.com>
2017-01-12 01:17:53 +0000

    Improved faceit aimbot (#435)
    * Improved faceit aimbot
    
    * Removed uneeded slider

--- src/settings.h
+++ src/settings.h
@@ -164,7 +164,6 @@ namespace Settings
 		extern bool enabled;
 		extern bool silent;
 		extern bool faceit;
-		extern float system_sens;
 		extern bool friendly;
 		extern int bone;
 		extern ButtonCode_t aimkey;
@@ -261,7 +260,6 @@ namespace Settings
 			float smoothAmount, smoothSaltMultiplier, errorMarginValue, autoAimFov, aimStepValue, rcsAmount, autoWallValue;
 			bool autoPistolEnabled, autoShootEnabled, autoScopeEnabled, noShootEnabled, ignoreJumpEnabled, smoke_check, autoWallEnabled, autoWallBones[6], autoAimRealDistance;
 			bool faceit;
-			float system_sens;
 
 			Weapon(bool enabled, bool silent, bool friendly, int bone, ButtonCode_t aimkey, bool aimkey_only,
 				   bool smoothEnabled, float smoothValue, int smoothType, bool smoothSaltEnabled, float smoothSaltMultiplier,
@@ -270,7 +268,7 @@ namespace Settings
 				   bool rcsEnabled, bool rcsAlways_on, float rcsFloat,
 				   bool autoPistolEnabled, bool autoShootEnabled, bool autoScopeEnabled,
 				   bool noShootEnabled, bool ignoreJumpEnabled, bool smoke_check, bool autoWallEnabled, float autoWallValue, bool autoWallBones[6], bool autoAimRealDistance,
-				   bool faceit, float system_sens)
+				   bool faceit)
 			{
 				this->enabled = enabled;
 				this->silent = silent;
@@ -306,7 +304,6 @@ namespace Settings
 
 				this->autoAimRealDistance = autoAimRealDistance;
 				this->faceit = faceit;
-				this->system_sens = system_sens;
 			}
 
 			Weapon() {};
